a3579819cec8e137e8779d0f2feb357986d9124f
Hamza Bouzid <hamzadeveloper1@gmail.com>
2022-03-04 20:33:48 +0100

    work in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,27 @@
-@dump($lista_movies[0]);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <title>Document</title>
+</head>
+<body>
+  <div class="cards">
+    
+      @foreach ($lista_movies; as $movie)
+      <div class="card">
+        <ul>   
+            <li><span>Titolo:</span>  {{$movie->title}}</li>
+            <li><span>Titolo Originale:</span> {{$movie->original_title}}</li>
+            <li><span>Lingua:</span> {{$movie->nationality}}</li>
+            <li><span>Voto:</span> {{$movie->vote}}</li>
+        </ul>
+      </div>
+      @endforeach
+    
+  </div>
+  
+</body>
+</html>
